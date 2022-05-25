d4b161264c71a00a484bc3120676a568e0d819d7
ehsomma <ehsomma@gmail.com>
2022-05-25 11:21:55 -0300

    Change imports path because slither in CI need the full path.

--- contracts/NatalArtist.sol
+++ contracts/NatalArtist.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT.
 pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title NFT ERC-721. (for research porpose).
 /// @author Esteban H. Somma.
